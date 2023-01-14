c3ce74929fb5179c4dba63be6a1ea77faeb7ccfe
Wout Stiens <71498452+StiensWout@users.noreply.github.com>
2023-01-14 14:52:04 +0100

    push

--- StreaMe-Tests/src/features/delete-account.feature
+++ StreaMe-Tests/src/features/delete-account.feature
@@ -1,4 +1,4 @@
-Feature: Account verwijderen
+Feature: Account aanpassen
 
 als gebruiker wil ik mijn account kunnen verwijderen zodat ik niet meer op de applicatie kan. 
 
@@ -8,3 +8,18 @@ als gebruiker wil ik mijn account kunnen verwijderen zodat ik niet meer op de ap
     And ik op de knop verwijder account klik
     Then wordt mijn account verwijderd
     And wordt ik uitgelogd
+
+
+    Scenario: Account private zetten
+    Given ik ben ingelogd
+    When ik ben op de pagina van mijn profiel
+    And ik op de knop account private maken klik
+    Then wordt mijn account op private gezet
+    And kunnen alleen mensen die ik volg mijn profiel zien
+
+    Scenario: Account public zetten
+    Given ik ben ingelogd
+    When ik ben op de pagina van mijn profiel
+    And ik op de knop account public maken klik
+    Then wordt mijn account op public gezet
+    And kunnen mensen die mij niet volgen mijn profiel zien
--- StreaMe-Tests/src/steps/delete-account.steps.ts
+++ StreaMe-Tests/src/steps/delete-account.steps.ts
@@ -5,10 +5,12 @@ const feature = loadFeature('src/features/delete-account.feature')
 defineFeature(feature, test => {
     let accountDeleted = false;
     let loggidIn = false;
+    let accountPrivate = false;
 
     beforeEach(() => {
         accountDeleted = false;
         loggidIn = false;
+        accountPrivate = false;
     })
 
     test('A user wants to delete his account', ({ given, when, then, and }) => {
@@ -36,6 +38,58 @@ defineFeature(feature, test => {
         });
 
     });
+
+    test('A user wants to set his account to private', ({ given, when, then, and }) => {
+            
+            given('ik ben ingelogd', () => {
+                loggidIn = true;
+    
+            })
+
+            when('ik ben op de pagina van mijn profiel', () => {
+                //Page definition
+            })
+
+            and('ik op de knop maak account privé klik', () => {
+                accountPrivate = true;
+            })
+
+            then('wordt mijn account privé', () => {
+                expect(accountPrivate).toBe(true);
+            })
+
+            and('word ik uitgelogd', () => {
+                expect(loggidIn).toBe(false);
+    
+            });
+    })
+
+    test('A user wants to set his account to public', ({ given, when, then, and }) => {
+
+        given('ik ben ingelogd', () => {
+            loggidIn = true;
+
+        })
+
+        when('ik ben op de pagina van mijn profiel', () => {
+            //Page definition
+        })
+
+        and('ik op de knop maak account publiek klik', () => {
+            accountPrivate = false;
+        })
+
+        then('wordt mijn account publiek', () => {
+            expect(accountPrivate).toBe(false);
+        })
+
+        and('word ik uitgelogd', () => {
+            expect(loggidIn).toBe(false);
+
+        });
+
+    })
+
 });
 
 
